d507ea97a68e2f718a45b00158bad7463553d032
uKaxg <uka.pgr@gmail.com>
2024-02-10 17:00:50 +0700

    Add morgan middleware to chart route

--- routes/chart.js
+++ routes/chart.js
@@ -1,5 +1,7 @@
+const morgan = require("morgan");
 const router = require("express").Router();
 const ChartController = require("../controllers/chartController");
+router.use(morgan("combined"));
 router.post("/", ChartController.get);
 router.post("/number", ChartController.create);
 module.exports = router;
